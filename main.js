4d1709509d760ba841b95f0801ae48c3a5fad049
Phong Truong <phongtruong@Phongs-MacBook-Pro.local>
2021-09-19 22:13:54 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// popover
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
+
+// Gender Select
+if (window.location.pathname === "/") {
+  const radioBtn1 = document.querySelector("#flexRadioDefault1");
+  const radioBtn2 = document.querySelector("#flexRadioDefault2");
+  const radioBtn3 = document.querySelector("#flexRadioDefault3");
+  const genderSelect = document.querySelector("#genderSelect");
+
+  radioBtn1.addEventListener("change", () => {
+    genderSelect.classList.add("d-none");
+  });
+  radioBtn2.addEventListener("change", () => {
+    genderSelect.classList.add("d-none");
+  });
+  radioBtn3.addEventListener("change", () => {
+    genderSelect.classList.remove("d-none");
+  });
+}
